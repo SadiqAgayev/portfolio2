0b18ef279646b0b8cbc8e4057046a2c35ce22a07
SadiqAgayev <sadiqagayev820@gmail.com>
2023-02-14 08:16:17 +0400

    features

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Banner from "./components/banner/Banner";
+import Features from "./components/features/Features";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div className="max-w-screen-2xl mx-auto px-16">
         <Navbar />
         <Banner />
+        <Features />
       </div>
     </div>
   );
---
+++ src/components/features/Features.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Title from "../layouts/Title";
+import Card from "./Card";
+import { AiFillAppstore } from "react-icons/ai";
+import { SiAntdesign, SiProgress } from "react-icons/si";
+import { FaBars, FaGlobe, FaMobile } from "react-icons/fa";
+
+const Features = () => {
+  return (
+    <section
+      id="features"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center text-center">
+        <Title title="Features" des="What I Do" />
+      </div>
+      <div className="grid grid-cols-3 gap-20">
+        <Card
+          title="Business Stratagy"
+          des="Lorem ipsum dolor sit amet consestetur adipisicing elit. Atque soluta hic consequuntur eum repellendus ad."
+          icon={<FaBars />}
+        />
+        <Card
+          title="App Development"
+          des="Lorem ipsum dolor sit amet consestetur adipisicing elit. Atque soluta hic consequuntur eum repellendus ad."
+          icon={<AiFillAppstore />}
+        />
+        <Card
+          title="SEO Optimisation"
+          des="Lorem ipsum dolor sit amet consestetur adipisicing elit. Atque soluta hic consequuntur eum repellendus ad."
+          icon={<SiProgress />}
+        />
+        <Card
+          title="Mobile Development"
+          des="Lorem ipsum dolor sit amet consestetur adipisicing elit. Atque soluta hic consequuntur eum repellendus ad."
+          icon={<FaMobile />}
+        />
+        <Card
+          title="UX Design"
+          des="Lorem ipsum dolor sit amet consestetur adipisicing elit. Atque soluta hic consequuntur eum repellendus ad."
+          icon={<SiAntdesign />}
+        />
+        <Card
+          title="Hosting Websites"
+          des="Lorem ipsum dolor sit amet consestetur adipisicing elit. Atque soluta hic consequuntur eum repellendus ad."
+          icon={<FaGlobe />}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Features;
