bea10d2be2d8ff3495eae291f40bdf02800391b2
SadiqAgayev <sadiqagayev820@gmail.com>
2023-02-21 16:53:21 +0400

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { logo } from "../../assets";
+
+const Footer = () => {
+  return (
+    <div className="w-full py-20 h-auto border-b-[1px] border-b-black grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-4 gap-8 px-6 mdl:px-10 lg:px-16">
+      <div className="w-full h-full flex flex-col gap-8">
+        <div>
+          <img className="w-32 h-32 object-cover rounded-full" src={logo} alt="logo" />
+        </div>
+        <div className="flex gap-4">
+          <span className="bannerIcon">
+            <FaFacebookF />
+          </span>
+          <span className="bannerIcon">
+            <FaTwitter />
+          </span>
+          <span className="bannerIcon">
+            <FaLinkedinIn />
+          </span>
+        </div>
+      </div>
+      <div className="w-full h-full">
+        <h3 className="text-xl uppercase text-red-700 tracking-wider">
+          Quick Link
+        </h3>
+        <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              About
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Portfolio
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Services
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Blog
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Contact
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full h-full">
+        <h3 className="text-xl uppercase text-red-700 tracking-wider">
+          Resources
+        </h3>
+        <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Authentication
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              System Status
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Terms of Service
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Pricing
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Over Right
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full h-full">
+        <h3 className="text-xl uppercase text-red-700 tracking-wider">
+          Developers
+        </h3>
+        <ul className="flex flex-col gap-4 font-titleFont font-medium py-6 overflow-hidden">
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Documentation
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Authenrication
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              API Reference
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Support
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+          <li>
+            <span className="w-full text-lg hover:text-red-700 duration-300 cursor-pointer relative group">
+              Open Source
+              <span className="absolute h-[1px] w-0 inline-flex bg-red-700 -bottom-1 left-0 group-hover:w-full duration-300"></span>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/footer/FooterBottom.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FooterBottom = () => {
+  return (
+    <div className="w-full py-10 px-6 mdl:px-10 lg:px-16">
+      <p className="text-center text-gray-500 text-base">
+        © 2023. All rights reserved by Sadiq Aghayev
+      </p>
+    </div>
+  );
+};
+
+export default FooterBottom;
